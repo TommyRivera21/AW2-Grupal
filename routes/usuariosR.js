40a9f63e68ebc1706ab55e7ac2738a7d9ae671df
Julio Casanova <104231741+JulioCasanova99@users.noreply.github.com>
2022-05-22 23:24:16 -0500

    Hola Mundo

--- routes/usuariosR.js
+++ routes/usuariosR.js
@@ -10,10 +10,10 @@ const { validacionCampos } = require('../middlewares');
 
 const router  = Router();
 
-/* router.get('/', obtenerUsuarios );
-router.get('/:id' , check('id','El id no es valido').isMongoId()
-, validacionCampos  , obtenerUsuario );
-router.post('/', check('nombre','El nombre es requerido').not().isEmpty() 
-, validacionCampos ,crearUsuario); */
+router.get('/', obtenerUsuarios );
+router.get('/:id' , check('id','El id no es valido').isMongoId(), 
+validacionCampos  , obtenerUsuario );
+router.post('/', check('nombre','El nombre es requerido').not().isEmpty(), 
+validacionCampos ,crearUsuario); 
 
 module.exports = router;
