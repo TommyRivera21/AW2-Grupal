df5b98e3f70baa6b883571b1fafc5999b1e1ceb3
Valentinpico <kwnigpico@gmail.com>
2022-05-23 15:25:23 -0500

    restaurando Carpeta appConsole de Elian

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-
+const inquirer= require('inquirer')
 require("dotenv").config();
 const Server = require("./server");
 const server = new Server();
 server.listen();
 
-// CODIGO PARA INICIAR LA APP DE CONSOLA
 
-/* const run = async () => {
-  const server = new Server();
-  server.listen();
-  app.run();
-};
-
-run(); */
 
