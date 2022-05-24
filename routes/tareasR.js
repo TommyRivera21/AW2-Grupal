d3bba03f45cb27f8f4c4ecd34b98e90e99a473a7
TommyRivera21 <e1316556743@live.uleam.edu.ec>
2022-05-23 19:02:24 -0500

    Se agrego funcion de consulta de tareas aceptadas de el usuario

--- routes/tareasR.js
+++ routes/tareasR.js
@@ -3,10 +3,22 @@ const { Router } = require('express');
 
 const router = Router();
 
-const { consultarTareasAceptadas } = require('../controllers').Tareas;
+//ruta del controlador para todas las tareas
+const { consultarTareas } = require('../controllers').Tareas;
+
+//ruta del controlador para las tareas aceptadas
+const { consultarTareasAceptadasUsuario } = require('../controllers').Tareas;
 
 const {validacionTareas} = require('../middlewares');
 
-router.get('/:idU',validacionTareas, consultarTareasAceptadas)
+//ruta de todas las tarea
+router.get('/:idU',validacionTareas, consultarTareas)
+
+//ruta de todas las tareas aceptadas de el usuario
+router.get('/:idU',validacionTareas, consultarTareasAceptadasUsuario)
+
+
+
+
 
 module.exports = router;
